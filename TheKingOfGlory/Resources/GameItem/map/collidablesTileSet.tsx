70610be18019ed2c96240d8204a33609780b2a23
bugggggggg <1851494@tongji.edu.cn>
2019-05-09 23:35:09 +0800

    add map

---
+++ TheKingOfGlory/Resources/GameItem/map/collidablesTileSet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="collidablesTileSet" tilewidth="64" tileheight="64" tilecount="3" columns="3">
+ <image source="collidablesTileSet.png" width="192" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ TheKingOfGlory/Resources/GameItem/map/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="grass" tilewidth="64" tileheight="64" tilecount="6" columns="3">
+ <image source="db7e0477b6404e158877ee53574b9b1a.png" width="222" height="134"/>
+</tileset>
